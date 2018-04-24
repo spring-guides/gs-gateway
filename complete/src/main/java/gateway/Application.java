0725134e44718b3a28be447f08959424247e8ae0
Greg Turnquist <gturnquist@pivotal.io>
2018-04-24 16:35:38 -0500

    Polishing

--- complete/src/main/java/gateway/Application.java
+++ complete/src/main/java/gateway/Application.java
@@ -21,25 +21,38 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    // tag::route-locator[]
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
         String httpUri = uriConfiguration.getHttpbin();
-        return builder.routes().
-                route(p -> p.path("/get").filters(f -> f.addRequestHeader("Hello", "World")).uri(httpUri)).
-                route(p -> p.host("*.hystrix.com").filters(f ->
-                        f.hystrix(config -> config.setName("mycmd").setFallbackUri("forward:/fallback"))).
-                        uri(httpUri)).
-                build();
+        return builder.routes()
+            .route(p -> p
+                .path("/get")
+                .filters(f -> f.addRequestHeader("Hello", "World"))
+                .uri(httpUri))
+            .route(p -> p
+                .host("*.hystrix.com")
+                .filters(f -> f
+                    .hystrix(config -> config
+                        .setName("mycmd")
+                        .setFallbackUri("forward:/fallback")))
+                .uri(httpUri))
+            .build();
     }
+    // end::route-locator[]
 
+    // tag::fallback[]
     @RequestMapping("/fallback")
     public Mono<String> fallback() {
         return Mono.just("fallback");
     }
+    // end::fallback[]
 }
 
+// tag::uri-configuration[]
 @ConfigurationProperties
 class UriConfiguration {
+    
     private String httpbin = "http://httpbin.org:80";
 
     public String getHttpbin() {
@@ -50,3 +63,4 @@ class UriConfiguration {
         this.httpbin = httpbin;
     }
 }
+// end::uri-configuration[]
--- complete/src/test/java/gateway/ApplicationTest.java
+++ complete/src/test/java/gateway/ApplicationTest.java
@@ -1,22 +1,21 @@
 package gateway;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * @author Ryan Baxter
  */
+// tag::code[]
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 		properties = {"httpbin=http://localhost:${wiremock.server.port}"})
@@ -30,11 +29,29 @@ public class ApplicationTest {
 	public void contextLoads() throws Exception {
 		//Stubs
 		stubFor(get(urlEqualTo("/get"))
-				.willReturn(aResponse().withBody("{\"headers\":{\"Hello\":\"World\"}}").withHeader("Content-Type", "application/json")));
-		stubFor(get(urlEqualTo("/delay/3")).willReturn(aResponse().withBody("no fallback").withFixedDelay(3000)));
+				.willReturn(aResponse()
+					.withBody("{\"headers\":{\"Hello\":\"World\"}}")
+					.withHeader("Content-Type", "application/json")));
+		stubFor(get(urlEqualTo("/delay/3"))
+			.willReturn(aResponse()
+				.withBody("no fallback")
+				.withFixedDelay(3000)));
 
-		webClient.get().uri("/get").exchange().expectStatus().isOk().expectBody().jsonPath("$.headers.Hello").isEqualTo("World");
-		webClient.get().uri("/delay/3").header("Host", "www.hystrix.com").exchange().expectStatus().isOk().expectBody().consumeWith(
-				response -> Assertions.assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
+		webClient
+			.get().uri("/get")
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$.headers.Hello").isEqualTo("World");
+
+		webClient
+			.get().uri("/delay/3")
+			.header("Host", "www.hystrix.com")
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.consumeWith(
+				response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
 	}
 }
+// end::code[]
