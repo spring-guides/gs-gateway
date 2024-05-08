0f3edeaddedd100e63b319a7d04be112b0e6f3fd
Robert McNees <rmcnees@vmware.com>
2024-05-08 07:13:56 -0400

    Updated dependencies to match what is provided from Spring Initializr

--- complete/src/test/java/gateway/ApplicationTest.java
+++ complete/src/test/java/gateway/ApplicationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
